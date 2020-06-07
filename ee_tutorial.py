49690313468c797c6ab66992ab94cb42ece1b40e
Carl Haines <carl.haines@pgr.reading.ac.uk>
2020-06-07 18:05:18 +0100

    Focus only on Reading area

--- ee_tutorial.py
+++ ee_tutorial.py
@@ -1,5 +1,8 @@
 import webbrowser
 import ee
+import matplotlib.pyplot as plt
+from skimage.io import imread
+import os
 
 ee.Initialize()
 image = ee.Image('srtm90_v4')
@@ -12,7 +15,7 @@ image = ee.Image('LANDSAT/LC08/C01/T1_TOA/LC08_044034_20140318'); #Landsat 8 ima
 # Access a collection
 collection = 'LANDSAT/LC08/C01/T1_TOA' #Landsat 7 raw images collection
 
-#Landsat_composite in Dresden area
+
 area_reading = list([(13.6, 50.96), (13.9, 50.96), (13.9, 51.12), (13.6, 51.12), (13.6, 50.96)])
 area_reading = ee.Geometry.Polygon(area_reading)
 time_range_reading = ['2014-07-28', '2014-08-05']
@@ -20,27 +23,6 @@ time_range_reading = ['2014-07-28', '2014-08-05']
 collection_reading = ('LANDSAT/LC08/C01/T1')
 print(type(area_reading))
 
-#Population density in Switzerland
-list_swiss = list([(6.72, 47.88),(6.72, 46.55),(9.72, 46.55),(9.72, 47.88),(6.72, 47.88)])
-area_swiss = ee.Geometry.Polygon(list_swiss)
-time_range_swiss=['2002-01-01', '2005-12-30']
-
-collection_swiss = ee.ImageCollection('CIESIN/GPWv4/population-density')
-print(type(collection_swiss))
-
-#Sentinel 2 cloud-free image in Zürich
-collection_zurich = ('COPERNICUS/S2')
-list_zurich = list([(8.53, 47.355),(8.55, 47.355),(8.55, 47.376),(8.53, 47.376),(8.53, 47.355)])
-area_zurich = ee.Geometry.Polygon(list_swiss)
-time_range_zurich = ['2018-05-01', '2018-07-30']
-
-
-#Landcover in Europe with CORINE dataset
-dataset_landcover = ee.Image('COPERNICUS/CORINE/V18_5_1/100m/2012')
-landCover_layer = dataset_landcover.select('landcover')
-print(type(landCover_layer))
-
-#Methods from climada.util.earth_engine module
 def obtain_image_landsat_composite(collection, time_range, area):
     """ Selection of Landsat cloud-free composites in the Earth Engine library
     See also: https://developers.google.com/earth-engine/landsat
@@ -112,17 +94,10 @@ def obtain_image_sentinel(collection, time_range, area):
     return sentinel_median
 
 composite_reading = obtain_image_landsat_composite(collection_reading, time_range_reading, area_reading)
-median_swiss = obtain_image_median(collection_swiss, time_range_swiss, area_swiss)
-zurich_median = obtain_image_sentinel(collection_zurich, time_range_zurich, area_zurich)
-
-#Selection of specific bands from an image
-zurich_band = zurich_median.select(['B4','B3','B2'])
-
 
 
 print(composite_reading.select(['B4','B3','B2']).getInfo())
-print(type(median_swiss))
-print(type(zurich_band))
+
 
 def get_region(geom):
     """Get the region of a given geometry, needed for exporting tasks.
@@ -145,8 +120,6 @@ def get_region(geom):
 
 
 region_reading = get_region(area_reading)
-region_swiss = get_region(area_swiss)
-region_zurich= get_region(area_zurich)
 
 def get_url(name, image, scale, region):
     """It will open and download automatically a zip folder containing Geotiff data of 'image'.
@@ -171,41 +144,18 @@ def get_url(name, image, scale, region):
     webbrowser.open_new_tab(path)
     return path
 
-
-
-url_swiss = get_url('swiss_pop', median_swiss, 900, region_swiss)
 url_reading = get_url('reading', composite_reading, 30, region_reading)
-url_landcover = get_url('landcover_swiss', landCover_layer, 100, region_swiss)
 
-#For the example of Zürich, due to size, it doesn't work on Jupyter Notebook but it works on Python
-#url_zurich = get_url('sentinel', zurich_band, 10, region_zurich)
-
-print(url_swiss)
 print(url_reading)
-print(url_landcover)
 
-import numpy as np
-from skimage import data
-import matplotlib.pyplot as plt
-from skimage.color import rgb2gray
-
-from skimage.io import imread
-from skimage import exposure
-from skimage.filters import try_all_threshold
-from skimage.filters import threshold_otsu, threshold_local
-from skimage import measure
-from skimage import feature
-
-import os
-#from climada.util import DATA_DIR
 DATA_DIR = "C:\\Users\\carlhaines\\Downloads"
 
-swiss_pop = os.path.join(DATA_DIR, 'demo/earth_engine', 'population-density_median.tif')
+
 reading = os.path.join(DATA_DIR, 'reading', 'reading.B1.tif') #B4 of Dresden example
-landcover = os.path.join(DATA_DIR, 'demo/earth_engine', 'landcover.tif')
-rgb_zurich = os.path.join(DATA_DIR, 'demo/earth_engine', 'rgb_zurich.tif') #created using gdal_merge.py
 
 print(reading)
+# TODO find way to unzip download file or keep image within python
+# Following should work for unzipped folder
 image_reading = imread(reading)
 plt.figure(figsize=(10, 10))
 plt.imshow(image_reading, cmap='gray', interpolation='nearest')
@@ -219,7 +169,8 @@ plt.imshow(image_reading_crop, cmap='gray', interpolation='nearest')
 plt.axis()
 plt.show()
 
-
+# TODO Debug following code to combine RGB bands
+"""
 from IPython.display import Image
 image = composite_reading.reduce('median')
 PERCENTILE_SCALE = 30  # Resolution in meters to compute the percentile at
@@ -234,3 +185,5 @@ reduction = image.visualize(bands= ['B4', 'B3', 'B2'],
                             min=list(reversed(minVal)), # reverse since bands are given in the other way (b2,b3,4b)
                             max=list(reversed(maxVal)),
                             gamma=1)
+                            
+"""
